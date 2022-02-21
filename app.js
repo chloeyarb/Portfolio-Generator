cc83b7501ffb48b4775428053a97810c48d4471a
Chloe Yarborough <chloeyarborough@Chloes-MacBook-Air.local>
2022-02-21 10:48:40 -0500

    update from reflection

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = profileDataArr => {
     //This...
@@ -10,6 +9,6 @@ const printProfileData = profileDataArr => {
 
     //Is the same as this...
     profileDataArr.forEach(profileItem => console.log(profileItem));
-}
+};
 
 printProfileData(profileDataArgs);
