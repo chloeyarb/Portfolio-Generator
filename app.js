7d3bed1329a343e512b29733ffc1ffbbed936fd3
Chloe Yarborough <chloeyarborough@Chloes-MacBook-Air.local>
2022-03-01 19:47:51 -0500

    inquire

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+console.log(inquirer);
+// const fs = require('fs');
 
-const generatePage = require('./src/page-template.js')
+// const generatePage = require('./src/page-template.js')
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('./index.html', pageHTML(name, github), err => {
+//     if (err) throw err;
 
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error (err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        }
+    ])
+    .then(answers => console.log(answers));
