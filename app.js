73550cb00bc7f3b420361f77e9bd0f2aa0387527
Chloe Yarborough <chloeyarborough@Chloes-MacBook-Air.local>
2022-03-06 09:28:40 -0500

    added validation to questions

--- app.js
+++ app.js
@@ -12,12 +12,105 @@ console.log(inquirer);
 //     console.log('Portfolio complete! Check out index.html to see the output!');
 // });
 
-inquirer
-    .prompt([
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?',
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log('Please enter your name!');
+                return false;
+              }              
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username. (Required)', 
+            validate: gitInput => {
+              if (gitInput) {
+                return true; 
+              } else {
+                console.log ('Please enter your GitHub username');
+              }
+            }
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:',
         }
-    ])
-    .then(answers => console.log(answers));
+    ]);    
+};
+
+const promptProject = () => {
+    console.log(`
+    =================
+    Add a new project
+    =================
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project? (Required)',
+            validate: projectName => {
+              if (projectName) {
+                return true;
+              } else {
+                console.log ('Please enter your project name');
+              }
+            }       
+          },
+          {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project. (Required)',
+            validate: descriptionInput => {
+              if (descriptionInput) {
+                return true;
+              } else {
+                console.log ('Please enter project description');
+              }
+            }
+          },
+          {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+          },
+          {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: linkInput => {
+              if (linkInput) {
+                return true;
+              } else {
+                console.log ('Please insert GitHub link');
+              }
+            }
+          },
+          {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+          },
+          {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+          }
+    ]);
+};
+promptUser()
+    .then(answers => console.log(answers))
+    .then(promptProject)
+    .then(projectAnswers => console.log(projectAnswers));
